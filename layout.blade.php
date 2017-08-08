64da2374a2a85c042ab4cf71c1adac276eb64635
Greg Hunt <freshbrewedweb@gmail.com>
2017-08-08 10:43:45 -0400

    Startering

---
+++ layout.blade.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Homepage</title>
+		<link rel="stylesheet" href="/assets/master.css">
+		<style>
+		body {
+			background: Red;
+		}
+		</style>
+	</head>
+	<body>
+		@yield('content')
+	</body>
+</html>
