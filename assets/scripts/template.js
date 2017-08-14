e71840c007051a90d116bd5b5de9fc37a846d236
Greg Hunt <freshbrewedweb@gmail.com>
2017-08-14 16:05:50 -0400

    Added jquery/bootstrap

---
+++ assets/scripts/template.js
@@ -0,0 +1,4 @@
+(function($) {
+	var title = $("title").text();
+	console.log(title + " is ready!");
+})( jQuery );
--- content/layout.blade.php
+++ content/layout.blade.php
@@ -32,10 +32,32 @@
 			</nav>
 		</header>
 		<div class="container">
+			<!-- Button trigger modal -->
+			<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#ctaModal">Call Now</button>
 			@yield('content')
 		</div>
 
-		<script src="/vendor/ubiweb/template-starter/scripts/main.js"></script>
+		<div class="modal fade" id="ctaModal" tabindex="-1" role="dialog">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <h4 class="modal-title">Modal title</h4>
+		      </div>
+		      <div class="modal-body">
+		        <p>One fine body&hellip;</p>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+		        <button type="button" class="btn btn-primary">Save changes</button>
+		      </div>
+		    </div><!-- /.modal-content -->
+		  </div><!-- /.modal-dialog -->
+		</div><!-- /.modal -->
+
+		<script src="/vendor/ubiweb/template-starter/assets/scripts/jquery-3.2.1.min.js"></script>
+		<script src="/vendor/ubiweb/template-starter/assets/vendor/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
+		<script src="/vendor/ubiweb/template-starter/assets/scripts/template.js"></script>
 		@yield('foot')
 	</body>
 </html>
